758c62ddb56701b16fac853eceb0bfafefbc0af5
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-03-21 16:41:26 +0200

    finish 1 task

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 import styles from "./searchForm.module.scss";
 
 class SearchForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     const { filter, onChange } = this.props;
 
     return (
-      <form className={styles.searchForm}>
+      <form className={styles.searchForm} onSubmit={this.handleSubmit}>
         <label>
           <h3 className={styles.searchForm__title}>Find contacts by name</h3>
           <input type="text" value={filter} onChange={onChange} />
